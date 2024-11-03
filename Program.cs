b7e13f26dedff7b38da31466ca9e5d87db5f3284
Michael <72989195+MIchael-Dolgov@users.noreply.github.com>
2024-11-03 14:35:15 +0300

    Completed Task 10

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task10
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            MyMaxBinaryHeap<int> MaxHeap1 = new MyMaxBinaryHeap<int>([19, 36, 17, 3, 25, 1, 2, 7]);
+            MyMaxBinaryHeap<int> MaxHeap2 = new MyMaxBinaryHeap<int>([100]);
+            foreach (int tmp in MaxHeap1.Nodes)
+            {
+                Console.Write(tmp);
+                Console.Write(" ");
+            }
+            MaxHeap1.Add(MaxHeap2.Nodes);
+            Console.WriteLine("\n");
+            foreach (int tmp in MaxHeap1.Nodes)
+            {
+                Console.Write(tmp);
+                Console.Write(" ");
+            } 
+        }
+    }
+}
