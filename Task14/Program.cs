ae55df467fb495ebe22f2f627dfea0961690fa44
Michael <72989195+MIchael-Dolgov@users.noreply.github.com>
2024-12-03 21:45:53 +0300

    Добавлен пример

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -1,12 +1,23 @@
-﻿using System;
-
-namespace Task14
+﻿namespace Task14
 {
     internal class Program
     {
         static void Main()
         {
-            
+            MyArrayDeque<double> myArrayDeque = new MyArrayDeque<double>();
+            double[] arr = { 1.0, 1.1, 1.2, 2.3, 4.5 };
+            myArrayDeque.AddAll(arr);
+            myArrayDeque.Add(10.1);
+            myArrayDeque.Add(22.11);
+            Console.WriteLine(myArrayDeque.GetFirst());
+            Console.WriteLine(myArrayDeque.GetLast());
+            Console.WriteLine(myArrayDeque.Pop());
+            Console.WriteLine(myArrayDeque.GetFirst());
+            Console.WriteLine(myArrayDeque.GetLast());
+            Console.WriteLine(myArrayDeque.Size());
+            myArrayDeque.Add(30.1);
+            Console.WriteLine(myArrayDeque.GetFirst());
+            Console.WriteLine(myArrayDeque.GetLast());
         }
     }
 }
