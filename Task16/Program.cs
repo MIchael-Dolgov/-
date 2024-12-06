f7a0700a833ff32d96f95412fb033cb430059870
Michael <72989195+MIchael-Dolgov@users.noreply.github.com>
2024-12-06 16:47:48 +0300

    Add files via upload

---
+++ Task16/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task16
+{
+    class Program
+    {
+        static void Main()
+        {
+            MyLinkedList<int> list = new MyLinkedList<int>();
+            list.Add(0, 1);
+            list.Add(1, 2);
+            list.Add(2, 3);
+            list.Print();
+            int[] array = { 4, 5, 6, 7, 8, 9, 10 };
+            list.AddAll(2, array);
+            list.AddLast(10);
+            list.AddLast(11);
+            list.AddLast(12);
+            list.AddLast(13);
+            list.AddLast(14); 
+            list.AddLast(15);
+            list.Print();
+            Console.WriteLine(list.RemoveFirstOccurrence(20));
+            list.Print();
+            Console.WriteLine(list.RemoveLastOccurrence(11));
+            list.Print();
+        }
+    }
+}
